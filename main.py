38563973ce5d42340a70e799c722edb4475f189c
ret7020 <ret7020@gmail.com>
2022-09-17 23:07:26 +0300

    Global ui finish

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
+
+
+class ResourcesStorage:
+    def __init__(self):
+        self.blueprint = Blueprint('resources', __name__, static_url_path='/users_data', static_folder='./data/users_data')
+        
+
 
 class Application:
     def __init__(self, name, host, port):
@@ -19,7 +26,9 @@ class Application:
         return render_template("login.html")
 
     def feed(self):
-        return render_template("feed.html")
+        user_name = "Stephan"
+        user_ava_path = '/users_data/avatars/default.png'
+        return render_template("feed.html", user_name=user_name, avatar_path=user_ava_path)
 
 
     
@@ -28,4 +37,6 @@ class Application:
 
 if __name__ == "__main__":
     app = Application(__name__, "0.0.0.0", "8080")
+    resources = ResourcesStorage()
+    app.app.register_blueprint(resources.blueprint)
     app.run()
